8fe31f2ca5268263679a24b0537b3c0bb9f2fd07
Victor Deryagin <vderyagin@gmail.com>
2016-07-02 13:50:27 +0300

    Add some tests for "oasps/insert-outline"

--- tests/test-insert-outline.el
+++ tests/test-insert-outline.el
@@ -4,4 +4,42 @@
       (oasps/insert-outline '("foo" "bar"))
       (expect (buffer-string)
               :to-equal
-              "* foo\n** bar"))))
+              "* foo\n** bar")))
+
+  (it "does nothing if said outline is already there"
+    (let ((text "* foo\n** bar\n*** baz"))
+      (with-org text
+        (oasps/insert-outline '("foo" "bar" "baz"))
+        (expect (buffer-string) :to-equal text))))
+
+  (it "is not confused by todo keywords"
+    (let ((text "* TODO foo\n** TODO bar\n*** DONE baz"))
+      (with-org text
+        (oasps/insert-outline '("foo" "bar" "baz"))
+        (expect (buffer-string) :to-equal text))))
+
+  (it "reuses existing partial outline"
+    (with-org "* foo\n** bar"
+      (oasps/insert-outline '("foo" "bar" "baz"))
+      (expect (buffer-string) :to-equal "* foo\n** bar\n*** baz")))
+
+  (it "leaves other siblings intact"
+    (let ((text "* foo
+** baz
+** quux
+*** grault
+*** plugh
+*** garply
+** foobar
+* bar
+** corge"))
+      (with-org text
+        (oasps/insert-outline '("foo" "quux" "plugh"))
+        (expect (buffer-string) :to-equal text))))
+
+  (it "does not produce any extra newlines when inserting heading"
+    (with-org "* foo\n* quux"
+      (oasps/insert-outline '("foo" "bar"))
+      (expect (buffer-string)
+              :to-equal
+              "* foo\n** bar\n* quux"))))
