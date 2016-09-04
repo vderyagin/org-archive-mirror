3c594b6d56f132ca69283ed14bfc244c773cd892
Victor Deryagin <vderyagin@gmail.com>
2016-09-04 17:27:41 +0300

    v0.2.0

--- org-archive-subtree-preserve-structure.el
+++ org-archive-subtree-preserve-structure.el
@@ -5,7 +5,7 @@
 ;; Author: Victor Deryagin <vderyagin@gmail.com>
 ;; Maintainer: Victor Deryagin <vderyagin@gmail.com>
 ;; Created: 30 Jun 2016
-;; Version: 0.1.1
+;; Version: 0.2.0
 
 ;; Package-Requires: ()
 
